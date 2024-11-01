38e4266b050805b57678845301f1f30bec8fbae4
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-11-01 23:47:35 +0600

    added sum function message

--- new.cpp
+++ new.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     int a, b;
-    cout << "Enter two numbers: ";
+    cout << "Enter two numbers sqeuntially" << endl;
     cin >> a >> b;
     cout << "Sum: " << a + b << endl;
     return 0;
