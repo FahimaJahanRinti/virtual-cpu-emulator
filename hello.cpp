54c04340ba97c91587a5a6ed1722035f97f68b76
Marzia <msmim0652@gmail.com>
2024-11-07 16:43:14 +0600

    marzia

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main() {
- cout << "Our github repo has been created!" << endl;
+ cout << "Our github repo has been created! marzia here" << endl;
+cout << "Our github repo has been created! marzia here" << endl;
+
     return 0;
 }   
